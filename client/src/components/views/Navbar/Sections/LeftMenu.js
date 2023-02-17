5abc893c6365d584d98b54f3ffdc30fb28bdfb3c
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-02-18 02:05:07 +0900

    navbar

---
+++ client/src/components/views/Navbar/Sections/LeftMenu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Menu} from 'antd';
+
+function LeftMenu(){
+    return(
+        <Menu>
+        <Menu.Item>
+            <a href="/login">Login</a>
+        </Menu.Item>
+        <Menu.Item>
+            <a href="/register">Register</a>
+        </Menu.Item>
+        </Menu>
+    )
+}
+
+export default LeftMenu;
