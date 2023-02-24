b2d9963998096953d64df3f5c1402c3d65cae025
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-02-24 19:00:03 +0900

    Card

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import KakaoMapApi from './Section/KakaoMapApi';
+import Card from './Section/Card';
+
+import sample from '../Image/HongTravel.png';
 function LandingPage(){
     return(
         
         <div>
-             <KakaoMapApi  />
+             <table>
+                <tr>
+                    <td><KakaoMapApi  /></td>
+                    <td>&nbsp; &nbsp; &nbsp;</td>
+                    <td>
+                        <Card img = {sample} title="제목" text="내용내용내용" />
+                        <Card img = {sample} title="제목22" text="내용222222" />
+                       
+                    </td>
+                </tr>
+             </table>
+             
         </div>
     )
 }
