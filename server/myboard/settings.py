2276039427e6e154e8ebd85a59e51529836ccd46
sdc05103 <heeni1004@gmail.com>
2023-03-09 13:36:48 +0900

    fixed

--- server/myboard/settings.py
+++ server/myboard/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+#우채윤 화이팅
 from pathlib import Path
 import os
 
